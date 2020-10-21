4a4282e9dc065bbe765fa67e1ca3c79ee75f25ad
André Luiz Melicio <71500309+andre-melicio@users.noreply.github.com>
2020-10-21 13:50:50 -0300

    up

--- pages/home.php
+++ pages/home.php
@@ -2,17 +2,20 @@
     <div style="background-image: url('<?php echo INCLUDE_PATH ?>img/banner-01.jpg')" class="slider-single"></div><!--slider-single-->
     <div style="background-image: url('<?php echo INCLUDE_PATH ?>img/banner-02.jpg')" class="slider-single"></div><!--slider-single-->
     <div style="background-image: url('<?php echo INCLUDE_PATH ?>img/banner-03.jpg')" class="slider-single"></div><!--slider-single-->
-    <div class="overlay"></div> <!-- overlay -->
-    <div class="bullets"></div>
-<div class="clear"></div>
 
-<div class="center box-cadastro">
+    <div class="overlay"></div> <!-- overlay -->
+
+    <div class="center box-cadastro">
         <form>
             <h2>Cadastre seu e-mail e receba nossas novidades!</h2>
             <input type="email" name="email" required>
             <input type="submit" name="acao" value="Cadastrar!">
         </form>
-</div>
+    </div>
+
+    <div class="bullets"></div>
+<div class="clear"></div>
+
 
 </section>
 
@@ -25,11 +28,17 @@
         <div class="w33 left box-projetos">
             <h3><img src="#" alt=""></h3>
             <h4>Titulo</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        <div class="w33 left box-projetos">
+            <h3><img src="#" alt=""></h3>
+            <h4>Titulo</h4>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
             </p>
         </div>
         <div class="w33 left box-projetos">
@@ -37,23 +46,45 @@
             <h4>Titulo</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-            </p>
-        </div>
-        <div class="w33 left box-projetos">
-            <h3><img src="#" alt=""></h3>
-            <h4>Titulo</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+
             </p>
         </div>
     </div> <!--center-->
     <div class="clear"></div>
     <div id="projetos" ></div>
 </section> <!--container-projetos-->
+
+<section class="container-autor">
+    <div class="center">
+        <h2 class="title">Especialiadades</h2>
+        <div class="w50 left">
+            <div class="box-especialidades">
+                <h3><i class="fab fa-js-square"></i></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et</p>
+            </div>
+            <div class="box-especialidades">
+                <h3><i class="fab fa-html5"></i></i></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et</p>
+            </div>
+            <div class="box-especialidades">
+                <h3><i class="fab fa-css3-alt"></i></i></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et</p>
+            </div>
+
+        </div>
+    </div> <!--center-->
+    <div class="w50 right box-img_autor">
+        <img src="<?php echo INCLUDE_PATH;?>img/autor.jpeg" alt="">
+        <h2>André L. Melicio</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+        </p>
+    </div><!--box-img_autor-->
+
+    <div class="clear"></div>
+    <div id="sobre" ></div>
+</section><!--container-autor-->
 
